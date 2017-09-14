8535f5c8da8c46c40b11dba43eeec291e7e21ba6
359056163 <DirtyPP2307521>
2017-09-15 01:00:41 +0800

    add grid

--- searchForm.js
+++ searchForm.js
@@ -5,6 +5,7 @@
 import React from 'react';
 import { Form } from 'antd';
 import FormItem from "antd/es/form/FormItem.d";
+import Row from "antd/es/grid/row.d";
 
 class SearchForm extends React.Component{
     handleSubmit = (e) => {
@@ -13,9 +14,38 @@ class SearchForm extends React.Component{
     }
     render(){
         return <Form>
-            <FormItem>
+            <Row>
+                <FormItem>
 
-            </FormItem>
+                </FormItem>
+                <FormItem>
+
+                </FormItem>
+            </Row>
+            <Row>
+                <FormItem>
+
+                </FormItem>
+                <FormItem>
+
+                </FormItem>
+            </Row>
+            <Row>
+                <FormItem>
+
+                </FormItem>
+                <FormItem>
+
+                </FormItem>
+            </Row>
+            <Row>
+                <FormItem>
+
+                </FormItem>
+                <FormItem>
+
+                </FormItem>
+            </Row>
         </Form>
     }
 }
