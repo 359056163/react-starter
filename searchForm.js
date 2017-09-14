f4d6d96c599ce88a89543c48905ec83914f0cd82
359056163 <DirtyPP2307521>
2017-09-15 00:53:34 +0800

    add file

---
+++ searchForm.js
@@ -0,0 +1,21 @@
+/**
+ * Created by puqin on 2017/9/14.
+ */
+
+import React from 'react';
+import { Form } from 'antd';
+import FormItem from "antd/es/form/FormItem.d";
+
+class SearchForm extends React.Component{
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
+    render(){
+        return <Form>
+            <FormItem>
+
+            </FormItem>
+        </Form>
+    }
+}
